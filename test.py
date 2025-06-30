39a2570af0aecd03425c967103c983d23a2bd311
capy-zhiao <uranusky1@gmail.com>
2025-06-30 15:44:01 -0400

    0630

--- test.py
+++ test.py
@@ -1,65 +1,39 @@
+from collections import deque
 from typing import List
 
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         result = []
-        length = len(nums)
-        print(nums)
+        window = deque()
 
-        for i in range(length - 2):
-            # 去重：跳过重复的 i
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
+        # 遍历 nums，每次更新窗口和结果
+        for i in range(len(nums)):
+            print("---------------{}-------------".format(i))
+            # 移除队首不在窗口范围内的元素（i - k）
+            if window and window[0] <= i - k:
+                print("1--", window)
+                window.popleft()
 
-            left = i + 1
-            right = length - 1
+            # 从队尾移除所有比当前 nums[i] 小的元素
+            while window and nums[window[-1]] < nums[i]:
+                print("2----nums[window[-1]]", nums[window[-1]])
+                print("2-----nums[i]", nums[i])
+                window.pop()
+                print("2----window", window)
 
-            print("i = %d, left = %d, right = %d" % (i, left, right))
+            # 将当前索引 i 加入队尾
+            window.append(i)
+            print("33333----window", window)
 
-            while left < right:
-                print("nums[i] = %d, nums[left] = %d, nums[right] = %d" % (nums[i], nums[left], nums[right]))
-                sum_all = nums[i] + nums[left] + nums[right]
-                print("sum_all = %d" % sum_all)
-                if sum_all > 0:
-                    right -= 1
-                    print("sum_all > 0, left = %d, right = %d" % (left, right))
-                    continue
-                elif sum_all < 0 :
-                    left += 1
-                    print("sum_all < 0, left = %d, right = %d" % (left, right))
-                    continue
-                else:
-                    triplet = [nums[i], nums[left], nums[right]]
-                    result.append(triplet)
-                    print(triplet)
-                    print(result)
-                    right -= 1
-                    left += 1
-                    break
-                    # # 找到一个三元组
-                    # result.append([nums[i], nums[left], nums[right]])
-                    # print(result)
-                    # # 跳过重复的 left 元素
-                    # while left < right and nums[left] == nums[left + 1]:
-                    #     left += 1
-                    #     print("left < right, left = %d, right = %d" % (left, right))
-                    #     print("nums[left] = %d" % nums[left])
-                    #     print("nums[left + 1] = %d" % nums[left + 1])
-                    # # 跳过重复的 right 元素
-                    # while left < right and nums[right] == nums[right - 1]:
-                    #     right -= 1
-                    #     print("right < left, right = %d, left = %d, right = %d" % (right, left, right))
-                    #     print("nums[right] = %d" % nums[right])
-                    #     print("nums[right - 1] = %d" % nums[right - 1])
-                    # # 移动指针继续查找
-                    # left += 1
-                    # right -= 1
-                    # print("left = %d" % left)
-                    # print("right = %d" % right)
-
+            # 当 i >= k - 1 时，把队首的 nums[索引] 加入 result
+            if i >= k - 1:
+                result.append(nums[window[0]])
+                print("4-----window", window)
+                print("4-----result", result)
+        print("last----result",result)
         return result
 
-my = Solution()
-print(my.threeSum([0, 0, 0, 0]))
+s = Solution()
+s.maxSlidingWindow(nums = [1,3,-1,-3,5,3,6,7], k = 3)
+s.maxSlidingWindow(nums = [1], k = 1)
