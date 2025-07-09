3f227ed802761d65875709af9ad8874e7ac35a79
capy-zhiao <uranusky1@gmail.com>
2025-07-09 14:50:50 -0400

    0709

--- test.py
+++ test.py
@@ -3,37 +3,26 @@ from typing import List
 
 
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        result = []
-        window = deque()
+    def reverse(self, chars, left, right):
+        while left < right:
+            chars[left], chars[right] = chars[right], chars[left]
+            left += 1
+            right -= 1
+        print(chars)
 
-        # 遍历 nums，每次更新窗口和结果
-        for i in range(len(nums)):
-            print("---------------{}-------------".format(i))
-            # 移除队首不在窗口范围内的元素（i - k）
-            if window and window[0] <= i - k:
-                print("1--", window)
-                window.popleft()
+    def reverseWords(self, s: str) -> str:
+        result = list(s.strip())
+        print(result)
 
-            # 从队尾移除所有比当前 nums[i] 小的元素
-            while window and nums[window[-1]] < nums[i]:
-                print("2----nums[window[-1]]", nums[window[-1]])
-                print("2-----nums[i]", nums[i])
-                window.pop()
-                print("2----window", window)
+        self.reverse(result, 0, len(result)-1)
 
-            # 将当前索引 i 加入队尾
-            window.append(i)
-            print("33333----window", window)
+        n = len(result)
+        start = 0
+        for end in range(n + 1):
+            if end == n or result[end] == ' ':
+                self.reverse(result, start, end - 1)
+                start = end + 1
 
-            # 当 i >= k - 1 时，把队首的 nums[索引] 加入 result
-            if i >= k - 1:
-                result.append(nums[window[0]])
-                print("4-----window", window)
-                print("4-----result", result)
-        print("last----result",result)
         return result
-
 s = Solution()
-s.maxSlidingWindow(nums = [1,3,-1,-3,5,3,6,7], k = 3)
-s.maxSlidingWindow(nums = [1], k = 1)
+s.reverseWords(s = "  hello world  ")
